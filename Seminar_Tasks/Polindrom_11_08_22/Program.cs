e2c52d0791fc1514f5538daa71bdc023c34e0930
Mikhail Konkov <kmapc4@gmail.com>
2022-08-11 13:46:24 +0300

    Палиндром

---
+++ Seminar_Tasks/Polindrom_11_08_22/Program.cs
@@ -0,0 +1,22 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        // Палиндром слово читающееся в обратном порядке с сохранением смысла.
+
+
+        Console.Clear();
+        Console.WriteLine("Введите слово , ");
+        string str = Convert.ToString(Console.ReadLine()).ToLower();
+        string str2 = ReverseString(str).ToLower();
+        if (str.Equals(str2)) Console.WriteLine("Палиндром");
+        else Console.WriteLine("Не палиндром");
+
+    }
+    public static string ReverseString(string str)
+    {
+        char[] c = str.ToCharArray();
+        Array.Reverse(c);
+        return new string(c);
+    }
+}
