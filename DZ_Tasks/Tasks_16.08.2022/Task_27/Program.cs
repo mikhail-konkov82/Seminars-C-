02ebae7c6c38f0ba88b8bbc271c402861d5e35eb
Mikhail Konkov <kmapc4@gmail.com>
2022-08-16 11:54:56 +0300

    Домашнее задание от 16.08.2022

---
+++ DZ_Tasks/Tasks_16.08.2022/Task_27/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.Clear();
+
+void Total(string str)
+{
+
+    int Sum = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        Sum = Sum + str[i] - '0';
+    }
+    Console.WriteLine("Сумма всех чисел: " + Sum);
+}
+
+Console.WriteLine("Введите число, ");
+string str = Console.ReadLine();
+Total(str);
+
+
+
+
+/*string str = "452";
+
+Console.WriteLine(str.ToCharArray().Select(ch => (int)ch -48).ToArray().Sum());*/
---
+++ DZ_Tasks/Tasks_16.08.2022/Task_29/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+Console.Clear();
+void Fill(int[] array)
+{
+    int m = array.Length;
+    for (int i = 0; i < m; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+        Console.Write(array[i] + ", ");
+    }
+
+}
+int m = 8;
+int[] array = new int[m];
+Fill(array);
