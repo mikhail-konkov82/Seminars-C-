55386ff86a99f60e0ed74a1c62b517b6c0ce0742
Mikhail Konkov <kmapc4@gmail.com>
2022-09-04 16:24:24 +0300

    DZ от 02.09.2022

---
+++ DZ_Tasks/Tasks_02.09.2022/Task_66/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.Clear();
+Console.WriteLine("Сумма натуральных элементов в промежутке от M до N.");
+Console.Write("M = ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("N = ");
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+
+Numbers(m);
+
+int Numbers(int m)
+{
+    if (m == n)
+    {
+        Console.WriteLine(sum += n);
+        return n;
+    }
+    else sum += m;
+
+    return Numbers(m + 1);
+
+}
---
+++ DZ_Tasks/Tasks_02.09.2022/Task_68/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 29   */
+
+Console.Clear();
+Console.Write("m = ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("n = ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"A({m},{n}) = " + Akkerman(m, n));
+// Если m= 0 , то n + 1
+// Если m != 0, а n = 0, то А(m-1, n)
+// Если n > 0 и  m > 0 , то ф-я А(m-1, A(m, n-1))
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else
+        if ((m != 0) && (n == 0))
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
